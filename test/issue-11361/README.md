f53bfc6e61f4acd27f42242dfbbcf31a8e2d910b
EliteMasterEric <ericmyllyoja@gmail.com>
2023-11-06 02:16:03 -0500

    Add issue-11361

---
+++ test/issue-11361/README.md
@@ -0,0 +1,3 @@
+# test/issue-11361
+
+An example demonstrating [haxe-foundation/haxe#11361](haxe-foundation/haxe#11361)
---
+++ test/issue-11361/src/test/CoolClass.hx
@@ -0,0 +1,9 @@
+package test;
+
+class CoolClass {
+    public function new() {}
+
+    public function toString():String {
+        return "Hello, world!";
+    }
+}
